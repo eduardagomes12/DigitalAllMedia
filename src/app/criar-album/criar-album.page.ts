f210928670b2bdce0e0b0b8adb57d872a30ec985
carolinamachado <carolina.machado@ipvc.pt>
2025-05-22 17:47:40 +0100

    pages standalone: false

--- src/app/criar-album/criar-album.page.ts
+++ src/app/criar-album/criar-album.page.ts
@@ -4,6 +4,7 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-criar-album',
   templateUrl: './criar-album.page.html',
   styleUrls: ['./criar-album.page.scss'],
+  standalone: false,
 })
 export class CriarAlbumPage implements OnInit {
 
