bfb0a88cf970d597b49880bcc5e1f9c73280039d
carolinamachado <carolina.machado@ipvc.pt>
2025-05-22 20:55:57 +0100

    pagina selecionar ficheiros atualizada

--- src/app/selecionar-ficheiros/selecionar-ficheiros.page.ts
+++ src/app/selecionar-ficheiros/selecionar-ficheiros.page.ts
@@ -1,27 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-selecionar-ficheiros',
   templateUrl: './selecionar-ficheiros.page.html',
   styleUrls: ['./selecionar-ficheiros.page.scss'],
-  standalone: false,
+  standalone:false,
 })
-export class SelecionarFicheirosPage {
+export class SelecionarFicheirosPage implements OnInit {
 
   segmento = 'recentes';
+  mostrarPesquisa = false;
+  textoPesquisa = '';
 
-  ficheiros = [
-    { thumbnail: 'assets/images/foto1.jpg', duracao: '00:50', selecionado: false },
-    { thumbnail: 'assets/images/foto2.jpg', duracao: '00:59', selecionado: false },
-    { thumbnail: 'assets/images/foto3.jpg', duracao: '01:10', selecionado: false },
-    { thumbnail: 'assets/images/foto4.jpg', selecionado: false },
-    { thumbnail: 'assets/images/foto5.jpg', selecionado: false },
-    { thumbnail: 'assets/images/foto6.jpg', selecionado: false },
-    // Podes adicionar mais imagens aqui
+  ficheirosRecentes = [
+    { thumbnail: 'assets/images/foto1.jpg', duracao: '00:50', selecionado: false, nome: 'Ferrari' },
+    { thumbnail: 'assets/images/foto2.jpg', duracao: '00:59', selecionado: false, nome: 'Hotel' },
+    { thumbnail: 'assets/images/foto3.jpg', duracao: '01:10', selecionado: false, nome: 'Neon City' },
+    { thumbnail: 'assets/images/foto4.jpg', selecionado: false, nome: 'Aquarium' },
+    { thumbnail: 'assets/images/foto5.jpg', selecionado: false, nome: 'Market' },
+    { thumbnail: 'assets/images/foto6.jpg', selecionado: false, nome: 'Exhibition' },
   ];
 
+  ficheirosTodos: any[] = []; // todos os ficheiros da aba ativa
+  ficheiros: any[] = [];      // ficheiros filtrados para mostrar
+
   constructor(private router: Router) {}
+
+  ngOnInit() {
+    this.atualizarFicheiros();
+  }
+
+  atualizarFicheiros() {
+    this.ficheirosTodos = this.segmento === 'recentes' ? this.ficheirosRecentes : [];
+    this.filtrarFicheiros();
+  }
+
+  filtrarFicheiros() {
+    const termo = this.textoPesquisa.toLowerCase();
+    this.ficheiros = this.ficheirosTodos.filter(f =>
+      f.nome.toLowerCase().includes(termo)
+    );
+  }
 
   alternarSelecao(index: number) {
     this.ficheiros[index].selecionado = !this.ficheiros[index].selecionado;
