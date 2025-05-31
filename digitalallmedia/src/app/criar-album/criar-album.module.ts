5e2691368664a2b8ca886c82c541250c8bf7720a
carolinamachado <carolina.machado@ipvc.pt>
2025-05-31 06:37:42 +0100

    Página criar albúm atualizada

--- digitalallmedia/src/app/criar-album/criar-album.module.ts
+++ digitalallmedia/src/app/criar-album/criar-album.module.ts
@@ -1,11 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-
+import { ReactiveFormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
-
 import { CriarAlbumPageRoutingModule } from './criar-album-routing.module';
-
 import { CriarAlbumPage } from './criar-album.page';
 
 @NgModule({
@@ -13,7 +11,8 @@ import { CriarAlbumPage } from './criar-album.page';
     CommonModule,
     FormsModule,
     IonicModule,
-    CriarAlbumPageRoutingModule
+    CriarAlbumPageRoutingModule,
+    ReactiveFormsModule
   ],
   declarations: [CriarAlbumPage]
 })
