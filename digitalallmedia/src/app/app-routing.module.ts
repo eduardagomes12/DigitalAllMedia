48b5610ce1420d6ac553f279dc4bd94dc6d06947
carolinamachado <carolina.machado@ipvc.pt>
2025-06-01 11:13:13 +0100

    Regista rota e componente da página de pesquisa

--- digitalallmedia/src/app/app-routing.module.ts
+++ digitalallmedia/src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
@@ -87,6 +88,177 @@ const routes: Routes = [
   }
 
 
+=======
+<<<<<<< HEAD
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  // ✅ Página inicial da app → Login
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full'
+  },
+  // ✅ Páginas do sistema (autenticação)
+  {
+    path: 'login',
+    loadChildren: () => import('./login/login.module').then(m => m.LoginPageModule)
+  },
+  {
+    path: 'signup',
+    loadChildren: () => import('./signup/signup.module').then(m => m.SignupPageModule)
+  },
+  {
+    path: 'termos',
+    loadChildren: () => import('./termos/termos.module').then(m => m.TermosPageModule)
+  },
+  {
+    path: 'recuperar-password',
+    loadChildren: () => import('./recuperar-password/recuperar-password.module').then(m => m.RecuperarPasswordPageModule)
+  },
+  {
+    path: 'verificar-codigo',
+    loadChildren: () => import('./verificar-codigo/verificar-codigo.module').then(m => m.VerificarCodigoPageModule)
+  },
+  {
+    path: 'nova-password',
+    loadChildren: () => import('./nova-password/nova-password.module').then(m => m.NovaPasswordPageModule)
+  },
+  // ✅ Tabs só acessíveis depois do login
+  {
+    path: 'tabs',
+    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  },
+  {
+    path: 'criar-album',
+    loadChildren: () => import('./criar-album/criar-album.module').then(m => m.CriarAlbumPageModule)
+  },
+  {
+    path: 'remover-album',
+    loadChildren: () => import('./remover-album/remover-album.module').then(m => m.RemoverAlbumPageModule)
+  },
+  {
+    path: 'editar-album',
+    loadChildren: () => import('./editar-album/editar-album.module').then(m => m.EditarAlbumPageModule)
+  },
+  {
+    path: 'editar-ficheiros',
+    loadChildren: () => import('./editar-ficheiros/editar-ficheiros.module').then(m => m.EditarFicheirosPageModule)
+  },
+  {
+    path: 'impressao',
+    loadChildren: () => import('./impressao/impressao.module').then(m => m.ImpressaoPageModule)
+  },
+  {
+    path: 'adicionar-arquivo',
+    loadChildren: () => import('./adicionar-arquivo/adicionar-arquivo.module').then(m => m.AdicionarArquivoPageModule)
+  },
+  {
+    path: 'selecionar-ficheiros',
+    loadChildren: () => import('./selecionar-ficheiros/selecionar-ficheiros.module').then(m => m.SelecionarFicheirosPageModule)
+  },
+  {
+    path: 'profile',
+    loadChildren: () => import('./profile/profile.module').then(m => m.ProfilePageModule)
+  },
+  {
+    path: 'escolher-musica',
+    loadChildren: () => import('./escolher-musica/escolher-musica.module').then(m => m.EscolherMusicaPageModule)
+  },
+  {
+    path: 'select-print',
+    loadChildren: () => import('./select-print/select-print.module').then( m => m.SelectPrintPageModule)
+  },
+  {
+  path: 'album-preview',
+  loadChildren: () => import('./album-preview/album-preview.module').then(m => m.AlbumPreviewPageModule)
+  }
+
+];
+=======
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  // ✅ Página inicial da app → Login
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full'
+  },
+  // ✅ Páginas do sistema (autenticação)
+  {
+    path: 'login',
+    loadChildren: () => import('./login/login.module').then(m => m.LoginPageModule)
+  },
+  {
+    path: 'signup',
+    loadChildren: () => import('./signup/signup.module').then(m => m.SignupPageModule)
+  },
+  {
+    path: 'termos',
+    loadChildren: () => import('./termos/termos.module').then(m => m.TermosPageModule)
+  },
+  {
+    path: 'recuperar-password',
+    loadChildren: () => import('./recuperar-password/recuperar-password.module').then(m => m.RecuperarPasswordPageModule)
+  },
+  {
+    path: 'verificar-codigo',
+    loadChildren: () => import('./verificar-codigo/verificar-codigo.module').then(m => m.VerificarCodigoPageModule)
+  },
+  {
+    path: 'nova-password',
+    loadChildren: () => import('./nova-password/nova-password.module').then(m => m.NovaPasswordPageModule)
+  },
+  // ✅ Tabs só acessíveis depois do login
+  {
+    path: 'tabs',
+    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  },
+  {
+    path: 'criar-album',
+    loadChildren: () => import('./criar-album/criar-album.module').then(m => m.CriarAlbumPageModule)
+  },
+  {
+    path: 'remover-album',
+    loadChildren: () => import('./remover-album/remover-album.module').then(m => m.RemoverAlbumPageModule)
+  },
+  {
+    path: 'editar-album',
+    loadChildren: () => import('./editar-album/editar-album.module').then(m => m.EditarAlbumPageModule)
+  },
+  {
+    path: 'editar-ficheiros',
+    loadChildren: () => import('./editar-ficheiros/editar-ficheiros.module').then(m => m.EditarFicheirosPageModule)
+  },
+  {
+    path: 'impressao',
+    loadChildren: () => import('./impressao/impressao.module').then(m => m.ImpressaoPageModule)
+  },
+  {
+    path: 'adicionar-arquivo',
+    loadChildren: () => import('./adicionar-arquivo/adicionar-arquivo.module').then(m => m.AdicionarArquivoPageModule)
+  },
+  {
+    path: 'selecionar-ficheiros',
+    loadChildren: () => import('./selecionar-ficheiros/selecionar-ficheiros.module').then(m => m.SelecionarFicheirosPageModule)
+  },
+  {
+    path: 'profile',
+    loadChildren: () => import('./profile/profile.module').then(m => m.ProfilePageModule)
+  },
+  {
+    path: 'escolher-musica',
+    loadChildren: () => import('./escolher-musica/escolher-musica.module').then(m => m.EscolherMusicaPageModule)
+  },  {
+    path: 'pesquisa',
+    loadChildren: () => import('./pesquisa/pesquisa.module').then( m => m.PesquisaPageModule)
+  }
+>>>>>>> a75069c (Regista rota e componente da página de pesquisa)
+
+>>>>>>> e6ef208 (Regista rota e componente da página de pesquisa)
 ];
 
 @NgModule({
--- digitalallmedia/src/app/app.module.ts
+++ digitalallmedia/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 
@@ -6,9 +6,9 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { IonicStorageModule } from '@ionic/storage-angular'; 
 import { HttpClientModule } from '@angular/common/http';     
 
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FiltrosModalModule } from './filtros-modal/filtros-modal.module';
 
 @NgModule({
   declarations: [AppComponent],
@@ -16,10 +16,12 @@ import { AppComponent } from './app.component';
     BrowserModule,
     IonicModule.forRoot(),
     AppRoutingModule,
-    HttpClientModule,            
-    IonicStorageModule.forRoot() 
+    HttpClientModule,
+    IonicStorageModule.forRoot(),
+    FiltrosModalModule
   ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AppModule {}
