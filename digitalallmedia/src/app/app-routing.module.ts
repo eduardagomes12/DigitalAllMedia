7ec93f865f3e1d6b3f6b1a4b75654a617e1f832d
carolinamachado <carolina.machado@ipvc.pt>
2025-06-02 14:52:44 +0100

    Páginas Atualizadas

--- digitalallmedia/src/app/app-routing.module.ts
+++ digitalallmedia/src/app/app-routing.module.ts
@@ -89,139 +89,35 @@ const routes: Routes = [
   },
   {
     path: 'delivery-details',
-    loadChildren: () => import('./delivery-details/delivery-details.module').then( m => m.DeliveryDetailsPageModule)
-  },
-  {
-    path: 'detalhes',
-    loadChildren: () => import('./detalhes/detalhes.module').then( m => m.DetalhesPageModule)
-  },
-  {
-    path: 'meus-albuns',
-    loadChildren: () => import('./meus-albuns/meus-albuns.module').then( m => m.MeusAlbunsPageModule)
-  },
-  {
-    path: 'cart',
-    loadChildren: () => import('./cart/cart.module').then( m => m.CartPageModule)
-  },
-  {
-    path: 'confirm-order',
-    loadChildren: () => import('./confirm-order/confirm-order.module').then( m => m.ConfirmOrderPageModule)
-  },  {
-    path: 'confirmar-encomenda',
-    loadChildren: () => import('./confirmar-encomenda/confirmar-encomenda.module').then( m => m.ConfirmarEncomendaPageModule)
-  },
-
-<<<<<<< HEAD
-];
-
-@NgModule({
-  imports: [RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })],
-  exports: [RouterModule]
-})
-export class AppRoutingModule {}
-=======
-const routes: Routes = [
-  {
-    path: '',
-    redirectTo: 'login',
-    pathMatch: 'full'
-  },
-  {
-    path: 'login',
-    loadChildren: () => import('./login/login.module').then(m => m.LoginPageModule)
-  },
-  {
-    path: 'signup',
-    loadChildren: () => import('./signup/signup.module').then(m => m.SignupPageModule)
-  },
-  {
-    path: 'termos',
-    loadChildren: () => import('./termos/termos.module').then(m => m.TermosPageModule)
-  },
-  {
-    path: 'recuperar-password',
-    loadChildren: () => import('./recuperar-password/recuperar-password.module').then(m => m.RecuperarPasswordPageModule)
-  },
-  {
-    path: 'verificar-codigo',
-    loadChildren: () => import('./verificar-codigo/verificar-codigo.module').then(m => m.VerificarCodigoPageModule)
-  },
-  {
-    path: 'nova-password',
-    loadChildren: () => import('./nova-password/nova-password.module').then(m => m.NovaPasswordPageModule)
-  },
-  {
-    path: 'tabs',
-    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
-  },
-  {
-    path: 'criar-album',
-    loadChildren: () => import('./criar-album/criar-album.module').then(m => m.CriarAlbumPageModule)
-  },
-  {
-    path: 'remover-album',
-    loadChildren: () => import('./remover-album/remover-album.module').then(m => m.RemoverAlbumPageModule)
-  },
-  {
-    path: 'editar-album',
-    loadChildren: () => import('./editar-album/editar-album.module').then(m => m.EditarAlbumPageModule)
-  },
-  {
-    path: 'editar-ficheiros',
-    loadChildren: () => import('./editar-ficheiros/editar-ficheiros.module').then(m => m.EditarFicheirosPageModule)
-  },
-  {
-    path: 'impressao',
-    loadChildren: () => import('./impressao/impressao.module').then(m => m.ImpressaoPageModule)
-  },
-  {
-    path: 'adicionar-arquivo',
-    loadChildren: () => import('./adicionar-arquivo/adicionar-arquivo.module').then(m => m.AdicionarArquivoPageModule)
-  },
-  {
-    path: 'selecionar-ficheiros',
-    loadChildren: () => import('./selecionar-ficheiros/selecionar-ficheiros.module').then(m => m.SelecionarFicheirosPageModule)
-  },
-  {
-    path: 'profile',
-    loadChildren: () => import('./profile/profile.module').then(m => m.ProfilePageModule)
-  },
-  {
-    path: 'escolher-musica',
-    loadChildren: () => import('./escolher-musica/escolher-musica.module').then(m => m.EscolherMusicaPageModule)
-  },
-  {
-    path: 'select-print',
-    loadChildren: () => import('./select-print/select-print.module').then(m => m.SelectPrintPageModule)
-  },
-  {
-    path: 'album-preview',
-    loadChildren: () => import('./album-preview/album-preview.module').then(m => m.AlbumPreviewPageModule)
-  },
-  {
-    path: 'select-print-type',
-    loadChildren: () => import('./select-print-type/select-print-type.module').then(m => m.SelectPrintTypePageModule)
-  },
-  {
-    path: 'pesquisa',
-    loadChildren: () => import('./pesquisa/pesquisa.module').then(m => m.PesquisaPageModule)
-  },
-  {
-    path: 'delivery-details',
     loadChildren: () => import('./delivery-details/delivery-details.module').then(m => m.DeliveryDetailsPageModule)
   },
   {
     path: 'detalhes',
     loadChildren: () => import('./detalhes/detalhes.module').then(m => m.DetalhesPageModule)
+  },
+  {
+    path: 'meus-albuns',
+    loadChildren: () => import('./meus-albuns/meus-albuns.module').then(m => m.MeusAlbunsPageModule)
+  },
+  {
+    path: 'cart',
+    loadChildren: () => import('./cart/cart.module').then(m => m.CartPageModule)
+  },
+  {
+    path: 'confirm-order',
+    loadChildren: () => import('./confirm-order/confirm-order.module').then(m => m.ConfirmOrderPageModule)
+  },
+  {
+    path: 'confirmar-encomenda',
+    loadChildren: () => import('./confirmar-encomenda/confirmar-encomenda.module').then(m => m.ConfirmarEncomendaPageModule)
   }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, {
     preloadingStrategy: PreloadAllModules,
-    onSameUrlNavigation: 'reload' // ✅ Força reload da página ao navegar para a mesma rota
+    onSameUrlNavigation: 'reload' // ✅ mantém navegação funcional mesmo na mesma rota
   })],
   exports: [RouterModule]
 })
 export class AppRoutingModule {}
->>>>>>> 683523a (Rotas Atualizadas)
