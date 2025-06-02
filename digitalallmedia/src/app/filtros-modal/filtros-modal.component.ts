4eaf19f9bcd781ac5f243e31067fb8af370441fe
carolinamachado <carolina.machado@ipvc.pt>
2025-06-02 14:13:40 +0100

    Paginas pesquisa + filtros

--- digitalallmedia/src/app/filtros-modal/filtros-modal.component.ts
+++ digitalallmedia/src/app/filtros-modal/filtros-modal.component.ts
@@ -1,7 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ModalController } from '@ionic/angular';
-import { IonicModule } from '@ionic/angular';
-
+import { SearchService } from 'src/services/search.service';
 
 @Component({
   selector: 'app-filtros-modal',
@@ -9,28 +8,45 @@ import { IonicModule } from '@ionic/angular';
   styleUrls: ['./filtros-modal.component.scss'],
   standalone: false,
 })
-export class FiltrosModalComponent {
+export class FiltrosModalComponent implements OnInit {
   tags: string = '';
   categoria: string = '';
   data: string = '';
+  categoriasDisponiveis: string[] = [];
 
-  constructor(private modalCtrl: ModalController) {}
+  constructor(
+    private modalCtrl: ModalController,
+    private searchService: SearchService
+  ) {}
 
-  aplicar() {
-    this.modalCtrl.dismiss({
-      tags: this.tags,
-      categoria: this.categoria,
-      data: this.data
+  ngOnInit(): void {
+    this.searchService.pesquisar('').subscribe(albuns => {
+      const categoriasDosAlbuns = albuns.map(a => a.categoria).filter(Boolean);
+
+      const categoriasSalvas = localStorage.getItem('categorias');
+      const categoriasLocais = categoriasSalvas ? JSON.parse(categoriasSalvas) : [];
+
+      const todasCategorias = [...categoriasDosAlbuns, ...categoriasLocais];
+      this.categoriasDisponiveis = Array.from(new Set(todasCategorias));
     });
   }
 
-  limpar() {
+  fechar(): void {
+    this.modalCtrl.dismiss();
+  }
+
+  limpar(): void {
     this.tags = '';
     this.categoria = '';
     this.data = '';
   }
 
-  fechar() {
-    this.modalCtrl.dismiss();
+  aplicar(): void {
+    const filtros = {
+      tags: this.tags,
+      categoria: this.categoria,
+      data: this.data
+    };
+    this.modalCtrl.dismiss(filtros);
   }
 }
